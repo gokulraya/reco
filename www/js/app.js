1c1277b845649180b0dd4b881543e18eacd33de2
Gokul Kumar Krishnan <gokulraya90@gmail.com>
2015-09-14 20:11:23 -0700

    Initialize repo

---
+++ www/js/app.js
@@ -0,0 +1,65 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+
+
+var app = angular.module('starter', ['ionic'])
+
+app.controller('recoCtrl', function($http, $scope){
+
+
+$scope.getMovie = function(response) {
+    $scope.movies = [];
+    var URL = 'http://www.tastekid.com/api/similar?k=127932-GoodRead-6VL3Z7F0&q= '+ $scope.val;
+    
+    $http.get(URL).success(function(response){
+      for(i=0;i<response.Similar.Results.length;i++) {
+        $scope.movies.push(response.Similar.Results[i])
+      }
+   })
+}
+})
+
+app.controller('listCtrl', function($http, $scope) {
+
+ $scope.data = [];
+ $scope.titleName = function(title) {
+  var details = 'http://www.omdbapi.com/?plot=short&t='+title;
+  $http.get(details).success(function(response) {
+    $scope.data.push(response);
+  })
+  console.log($scope.data);
+ }
+})
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'templates/list.html'
+  });
+ 
+ $stateProvider.state('details', {
+    url: '/details',
+    templateUrl: 'templates/details.html'
+  });
+
+  $urlRouterProvider.otherwise('/list');
+
+});
+
+
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
